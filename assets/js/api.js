d72b21372266daea9814549a418d68e8c607e6d4
Ningsang Jabegu <86409265+Ningsang-Jabegu@users.noreply.github.com>
2023-07-26 11:36:32 +0545

    svg photo added

--- assets/js/api.js
+++ assets/js/api.js
@@ -2,141 +2,141 @@ const categories = {
     characterDriven: [
         {
           name: "LYFE", 
-          img: "/assets/images/video_thumbnails/Home/video-1.png",
+          img: "/assets/images/video_thumbnails/Portfolio/Character_Driven/video-1.svg",
           src: "https://www.youtube.com/embed/oSYC2matVqI?autoplay=1&SameSite=None" 
         },
         {
           name: "Snoopy Cap", 
-          img: "/assets/images/video_thumbnails/Home/video-2.png",
+          img: "/assets/images/video_thumbnails/Portfolio/Character_Driven/video-2.svg",
           src: "https://www.youtube.com/embed/gN3-w8eeO7w?autoplay=1&SameSite=None" 
         },
         {
           name: "Realest", 
-          img: "/assets/images/video_thumbnails/Home/video-3.png" ,
+          img: "/assets/images/video_thumbnails/Portfolio/Character_Driven/video-3.svg" ,
           src: "https://www.youtube.com/embed/hQwGuQ0XOiA?autoplay=1&SameSite=None"
         },
         {
           name: "Soundtrace", 
-          img: "/assets/images/video_thumbnails/Home/video-4.png",
+          img: "/assets/images/video_thumbnails/Portfolio/Character_Driven/video-4.svg",
           src: "https://www.youtube.com/embed/6_oMDmJXIJA?autoplay=1&SameSite=None" 
         },
         {
           name: "Warmfuzzies", 
-          img: "/assets/images/video_thumbnails/Home/video-5.png",
+          img: "/assets/images/video_thumbnails/Portfolio/Character_Driven/video-5.svg",
           src: "https://www.youtube.com/embed/jadVaoCTeWU?autoplay=1&SameSite=None" 
         },
         {
           name: "Rekenix", 
-          img: "/assets/images/video_thumbnails/Home/video-6.png",
+          img: "/assets/images/video_thumbnails/Portfolio/Character_Driven/video-6.svg",
           src: "https://www.youtube.com/embed/W2CjinAKxbI?autoplay=1&SameSite=None" 
         },
         {
           name: "Rekenix", 
-          img: "/assets/images/video_thumbnails/Home/video-7.png",
+          img: "/assets/images/video_thumbnails/Portfolio/Character_Driven/video-7.svg",
           src: "https://www.youtube.com/embed/hQwGuQ0XOiA?autoplay=1&SameSite=None" //need original link
         },
         {
           name: "Brain Parlor", 
-          img: "/assets/images/video_thumbnails/Home/video-8.png",
+          img: "/assets/images/video_thumbnails/Portfolio/Character_Driven/video-8.svg",
           src: "https://www.youtube.com/embed/oSYC2matVqI?autoplay=1&SameSite=None" 
         },
         {
           name: "Signathon", 
-          img: "/assets/images/video_thumbnails/Home/video-9.png",
+          img: "/assets/images/video_thumbnails/Portfolio/Character_Driven/video-9.svg",
           src: "https://www.youtube.com/embed/feDeploBxAc?autoplay=1&SameSite=None" 
         },
       ],
       motionGraphics: [
         {
           name: "Cribl", 
-          img: "/assets/images/video_thumbnails/Portfolio/Character_Driven/video-1.png",
+          img: "/assets/images/video_thumbnails/Portfolio/Motion_Graphics/video-1.svg",
           src: "https://www.youtube.com/embed/UhOxp_dsuVE?autoplay=1&Samesite=None" 
         },
         {
           name: "Roche", 
-          img: "/assets/images/video_thumbnails/Portfolio/Character_Driven/video-2.png",
+          img: "/assets/images/video_thumbnails/Portfolio/Motion_Graphics/video-2.svg",
           src: "https://www.youtube.com/embed/XKTXMyAKts0?autoplay=1&Samesite=None" 
         },
         {
           name: "Hygiene", 
-          img: "/assets/images/video_thumbnails/Portfolio/Character_Driven/video-3.png",
+          img: "/assets/images/video_thumbnails/Portfolio/Motion_Graphics/video-3.svg",
           src: "https://www.youtube.com/embed/Fo-QOPvGgg4?autoplay=1&Samesite=None" 
         },
         {
           name: "Hyperguest", 
-          img: "/assets/images/video_thumbnails/Portfolio/Character_Driven/video-4.png",
+          img: "/assets/images/video_thumbnails/Portfolio/Motion_Graphics/video-4.svg",
           src: "https://www.youtube.com/embed/5W4CeGwQ-zc?autoplay=1&Samesite=None" 
         },
         {
           name: "Cribl", 
-          img: "/assets/images/video_thumbnails/Portfolio/Character_Driven/video-5.png",
+          img: "/assets/images/video_thumbnails/Portfolio/Motion_Graphics/video-5.svg",
           src: "https://www.youtube.com/embed/tIaodtDZjW8?autoplay=1&Samesite=None" 
         },
         {
           name: "Dispatch Pro", 
-          img: "/assets/images/video_thumbnails/Portfolio/Character_Driven/video-6.png",
+          img: "/assets/images/video_thumbnails/Portfolio/Motion_Graphics/video-6.svg",
           src: "https://www.youtube.com/embed/HC9sNigO6is?autoplay=1&Samesite=None" 
         },
         {
           name: "Vudini", 
-          img: "/assets/images/video_thumbnails/Portfolio/Character_Driven/video-7.png",
+          img: "/assets/images/video_thumbnails/Portfolio/Motion_Graphics/video-7.svg",
           src: "https://www.youtube.com/embed/Jzfio4aKR3c?autoplay=1&Samesite=None" 
         },
         {
           name: "Flex Charging", 
-          img: "/assets/images/video_thumbnails/Portfolio/Character_Driven/video-8.png",
+          img: "/assets/images/video_thumbnails/Portfolio/Motion_Graphics/video-8.svg",
           src: "https://www.youtube.com/embed/e7sUUcwfe9E?autoplay=1&Samesite=None" 
         },
         {
           name: "Charity token", 
-          img: "/assets/images/video_thumbnails/Portfolio/Character_Driven/video-9.png",
+          img: "/assets/images/video_thumbnails/Portfolio/Motion_Graphics/video-9.svg",
           src: "https://www.youtube.com/embed/Ahsgeb7Xm3c?autoplay=1&Samesite=None" 
         },
       ],
       whiteboardAnimation: [
         {
           name: "Mentor Ability", 
-          img: "/assets/images/video_thumbnails/Portfolio/Whiteboard_Animation/video-1.png",
+          img: "/assets/images/video_thumbnails/Portfolio/Whiteboard_Animation/video-1.svg",
           src: "https://www.youtube.com/embed/OS3eeDLPm-A?autoplay=1&Samesite=None" 
         },
         {
           name: "Stigg Security", 
-          img: "/assets/images/video_thumbnails/Portfolio/Whiteboard_Animation/video-2.png",
+          img: "/assets/images/video_thumbnails/Portfolio/Whiteboard_Animation/video-2.svg",
           src: "https://www.youtube.com/embed/ytm-HF8-LH8?autoplay=1&Samesite=None" 
         },
         {
           name: "Libido", 
-          img: "/assets/images/video_thumbnails/Portfolio/Whiteboard_Animation/video-3.png",
+          img: "/assets/images/video_thumbnails/Portfolio/Whiteboard_Animation/video-3.svg",
           src: "https://www.youtube.com/embed/MZcsQmhQ3vg?autoplay=1&Samesite=None" 
         },
         {
           name: "IDD Safe", 
-          img: "/assets/images/video_thumbnails/Portfolio/Whiteboard_Animation/video-4.png",
+          img: "/assets/images/video_thumbnails/Portfolio/Whiteboard_Animation/video-4.svg",
           src: "https://www.youtube.com/embed/NEvl9GswuFI?autoplay=1&Samesite=None" 
         },
         {
           name: "The Silk Route", 
-          img: "/assets/images/video_thumbnails/Portfolio/Whiteboard_Animation/video-5.png",
+          img: "/assets/images/video_thumbnails/Portfolio/Whiteboard_Animation/video-5.svg",
           src: "https://www.youtube.com/embed/niPynhBVBqg?autoplay=1&Samesite=None" 
         },
         {
           name: "Gateway Employment", 
-          img: "/assets/images/video_thumbnails/Portfolio/Whiteboard_Animation/video-6.png",
+          img: "/assets/images/video_thumbnails/Portfolio/Whiteboard_Animation/video-6.svg",
           src: "https://www.youtube.com/embed/86_pj439qiM?autoplay=1&Samesite=None" 
         },
         {
           name: "Sturdy", 
-          img: "/assets/images/video_thumbnails/Portfolio/Whiteboard_Animation/video-7.png",
+          img: "/assets/images/video_thumbnails/Portfolio/Whiteboard_Animation/video-7.svg",
           src: "https://www.youtube.com/embed/RvKezo1lOjg?autoplay=1&Samesite=None" 
         },
         {
           name: "Superjeweler", 
-          img: "/assets/images/video_thumbnails/Portfolio/Whiteboard_Animation/video-8.png",
+          img: "/assets/images/video_thumbnails/Portfolio/Whiteboard_Animation/video-8.svg",
           src: "https://www.youtube.com/embed/TJyOSbMFHZA?autoplay=1&Samesite=None" 
         },
         {
           name: "EU Whiteboard", 
-          img: "/assets/images/video_thumbnails/Portfolio/Whiteboard_Animation/video-9.png",
+          img: "/assets/images/video_thumbnails/Portfolio/Whiteboard_Animation/video-9.svg",
           src: "https://www.youtube.com/embed/KoMAmhlXjBg?autoplay=1&Samesite=None" 
         },
       ]
@@ -144,94 +144,94 @@ const categories = {
       isometric: [
         {
           name: "KFC Aus", 
-          img: "/assets/images/video_thumbnails/Portfolio/Isometric/video-1.png",
+          img: "/assets/images/video_thumbnails/Portfolio/Isometric/video-1.svg",
           src: "https://www.youtube.com/embed/o7N3vcRR7u0?autoplay=1&Samesite=None" 
         },
         {
           name: "Pahtomath", 
-          img: "/assets/images/video_thumbnails/Portfolio/Isometric/video-2.png",
+          img: "/assets/images/video_thumbnails/Portfolio/Isometric/video-2.svg",
           src: "https://www.youtube.com/embed/s3O29tDBYUM?autoplay=1&Samesite=None" 
         },
         {
           name: "Supplime", 
-          img: "/assets/images/video_thumbnails/Portfolio/Isometric/video-3.png",
+          img: "/assets/images/video_thumbnails/Portfolio/Isometric/video-3.svg",
           src: "https://www.youtube.com/embed/iYVbc8cFHbQ?autoplay=1&Samesite=None" 
         },
         {
           name: "Praqio", 
-          img: "/assets/images/video_thumbnails/Portfolio/Isometric/video-4.png",
+          img: "/assets/images/video_thumbnails/Portfolio/Isometric/video-4.svg",
           src: "https://www.youtube.com/embed/p5bzLGzCWO8?autoplay=1&Samesite=None" 
         },
         {
           name: "Atidot", 
-          img: "/assets/images/video_thumbnails/Portfolio/Isometric/video-5.png",
+          img: "/assets/images/video_thumbnails/Portfolio/Isometric/video-5.svg",
           src: "https://www.youtube.com/embed/ZCJh_1wA3u0?autoplay=1&Samesite=None" 
         },
         {
           name: "Club", 
-          img: "/assets/images/video_thumbnails/Portfolio/Isometric/video-6.png",
+          img: "/assets/images/video_thumbnails/Portfolio/Isometric/video-6.svg",
           src: "https://www.youtube.com/embed/nDxixSE9mMg?autoplay=1&Samesite=None" 
         },
         {
           name: "Rovjok", 
-          img: "/assets/images/video_thumbnails/Portfolio/Isometric/video-7.png",
+          img: "/assets/images/video_thumbnails/Portfolio/Isometric/video-7.svg",
           src: "https://www.youtube.com/embed/olvW7TKFRjM?autoplay=1&Samesite=None" 
         },
         {
           name: "MyLeanFactory", 
-          img: "/assets/images/video_thumbnails/Portfolio/Isometric/video-8.png",
+          img: "/assets/images/video_thumbnails/Portfolio/Isometric/video-8.svg",
           src: "https://www.youtube.com/embed/Cd1tx9pijc0?autoplay=1&Samesite=None" 
         },
         {
           name: "Insightfull", 
-          img: "/assets/images/video_thumbnails/Portfolio/Isometric/video-9.png",
+          img: "/assets/images/video_thumbnails/Portfolio/Isometric/video-9.svg",
           src: "https://www.youtube.com/embed/sCV6VnvJ5AA?autoplay=1&Samesite=None" 
         },
       ],
       softwareDemo: [
         {
           name: "Hi Touch", 
-          img: "/assets/images/video_thumbnails/Portfolio/Software_Demo/video-1.png",
+          img: "/assets/images/video_thumbnails/Portfolio/Software_Demo/video-1.svg",
           src: "https://www.youtube.com/embed/Icz6SiqAJPE?autoplay=1&Samesite=None" 
         },
         {
           name: "Draft Co.", 
-          img: "/assets/images/video_thumbnails/Portfolio/Software_Demo/video-2.png",
+          img: "/assets/images/video_thumbnails/Portfolio/Software_Demo/video-2.svg",
           src: "https://www.youtube.com/embed/DudkcYpKNcs?autoplay=1&Samesite=None" 
         },
         {
           name: "ProWriter", 
-          img: "/assets/images/video_thumbnails/Portfolio/Software_Demo/video-3.png",
+          img: "/assets/images/video_thumbnails/Portfolio/Software_Demo/video-3.svg",
           src: "https://www.youtube.com/embed/Q8b-mdcKbRA?autoplay=1&Samesite=None" 
         },
         {
           name: "LoTops", 
-          img: "/assets/images/video_thumbnails/Portfolio/Software_Demo/video-4.png",
+          img: "/assets/images/video_thumbnails/Portfolio/Software_Demo/video-4.svg",
           src: "https://www.youtube.com/embed/oZ145hbJYT8?autoplay=1&Samesite=None" 
         },
         {
           name: "Sign in", 
-          img: "/assets/images/video_thumbnails/Portfolio/Software_Demo/video-5.png",
+          img: "/assets/images/video_thumbnails/Portfolio/Software_Demo/video-5.svg",
           src: "https://www.youtube.com/embed/fJ7QS02Ccic?autoplay=1&Samesite=None" 
         },
         {
           name: "zipcan", 
-          img: "/assets/images/video_thumbnails/Portfolio/Software_Demo/video-6.png",
+          img: "/assets/images/video_thumbnails/Portfolio/Software_Demo/video-6.svg",
           src: "https://www.youtube.com/embed/l1z5nYjOKY0?autoplay=1&Samesite=None" 
         },
         {
           name: "Switch", 
-          img: "/assets/images/video_thumbnails/Portfolio/Software_Demo/video-7.png",
+          img: "/assets/images/video_thumbnails/Portfolio/Software_Demo/video-7.svg",
           src: "https://www.youtube.com/embed/u39G_2Xas3g?autoplay=1&Samesite=None" 
         },
         {
           name: "KOSA", 
-          img: "/assets/images/video_thumbnails/Portfolio/Software_Demo/video-8.png",
+          img: "/assets/images/video_thumbnails/Portfolio/Software_Demo/video-8.svg",
           src: "https://www.youtube.com/embed/4-ndfT-RRGs?autoplay=1&Samesite=None" 
         },
         {
           name: "Corporate fitness", 
-          img: "/assets/images/video_thumbnails/Portfolio/Software_Demo/video-9.png",
+          img: "/assets/images/video_thumbnails/Portfolio/Software_Demo/video-9.svg",
           src: "https://www.youtube.com/embed/KT49f1BI6ns?autoplay=1&Samesite=None" 
         },
       ]
